d510b8bfa45366e5e5e2e2c3a121adcbfb51ac07
Daisy <Daisy@DaisytekiMacBook-Air.local>
2022-06-04 16:43:37 -0700

    Madlib Step2&3

--- madlibs.py
+++ madlibs.py
@@ -51,6 +51,28 @@ def greet_person():
     return render_template("compliment.html", person=player, compliment=compliment)
 
 
+@app.route("/game")
+def show_madlib_form():
+    """ Show madlib form """
+
+    play_game = request.args.get("play")
+
+    if play_game == "yes":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
+@app.route("/madlib")
+def show_madlib():
+    name = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html", person=name, color=color, noun=noun, adjective=adjective)
+
+
+
 if __name__ == "__main__":
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
